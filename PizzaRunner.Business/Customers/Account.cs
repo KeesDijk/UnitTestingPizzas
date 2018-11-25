7feda6d2211bb3990c22b41334de8c1577f2be48
Administrator <Administrator@WIN-3E2N4PKEMHO>
2018-11-25 16:32:54 +0100

    Added business and account

---
+++ PizzaRunner.Business/Customers/Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PizzaRunner.Business.Customers
+{
+    public class Account
+    {
+        private readonly string _owner;
+        private double _balance;
+
+        public Account(string owner, double balance)
+        {
+            _owner = owner;
+            _balance = balance;
+        }
+
+        public double Balance()
+        {
+            return _balance;
+        }
+
+        public string Owner()
+        {
+            return _owner;
+        }
+
+        public void WithDraw(double amount)
+        {
+            if (amount < 0)
+                throw new ArgumentOutOfRangeException(nameof(amount), "cannot withdraw negative amount");
+
+            _balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount < 0)
+                throw new ArgumentOutOfRangeException(nameof(amount), "cannot deposit negative amount");
+
+            _balance += amount;
+        }
+    }
+}
---
+++ PizzaRunnerConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using PizzaRunner.Business.Customers;
+using System;
+
+namespace PizzaRunnerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account("Mario", 10000);
+
+            Console.WriteLine($"Created account for {account.Owner()}, with balance {account.Balance()}");
+
+            Console.ReadLine();
+        }
+    }
+}
